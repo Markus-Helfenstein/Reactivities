fd87ca5516dc0c81adc51dc2c0c91fb6246df304
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-04-25 09:45:57 +0200

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,20 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt => 
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+AddApplicationServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
@@ -52,3 +41,26 @@ catch (Exception ex)
 }
 
 app.Run();
+
+
+static IServiceCollection AddApplicationServices(IServiceCollection services, IConfiguration config)
+{
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddDbContext<DataContext>(opt => 
+    {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+    });
+    services.AddCors(opt => 
+    {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+    });
+    services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+    services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+    return services;
+}
